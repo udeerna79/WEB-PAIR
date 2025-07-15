f5708406c19f7478af7aa39de68f894abc620772
udeerna79 <nudeerna@gmail.com>
2025-07-15 19:56:08 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -82,7 +82,7 @@ router.get("/", async (req, res) => {
             );
 
             const sid = `*ZERO_BYTE [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
-            const mg = `🛑 *Do not share this code to anyone* 🛑`;
+            const mg = `🛑 *WELOCOME TO ZERO BOT* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
                 url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
